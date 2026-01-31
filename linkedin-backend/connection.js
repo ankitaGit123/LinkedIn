5cfee1f05860d6c5bfbbe47fccd516f77900d99d
Ankita <ankita0404.bgml@gmail.com>
2026-01-31 06:37:02 +0530

    Use MongoDB URI from env

--- linkedin-backend/connection.js
+++ linkedin-backend/connection.js
@@ -1,15 +1,11 @@
-const mongoose =require('mongoose');
+const mongoose = require("mongoose");
 
-// LinkedinClone
-
-// mongoose.connect("mongodb://localhost:27017/LinkedinClone").then(res=>{
-//     console.log("Database Successfully Connected")
-// }).catch(err=>{
-//     console.log("Database Connection Failed")
-// })
-
-mongoose.connect("mongodb+srv://ankitacodes25_db_user:5ofEcEBST6SwoZz5@linkedin.uohop0q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(res=>{
-    console.log("Database Successfully Connected")
-}).catch(err=>{
-    console.log("Database Connection Failed")
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Database Successfully Connected");
+  })
+  .catch((err) => {
+    console.log("Database Connection Failed");
+    console.log(err);
+  });
